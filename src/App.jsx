6b232b85d0b6d9b55891953371ee018b748770ec
Daniel <danny0988lewis@gmail.com>
2026-02-10 17:36:08 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -14,10 +14,10 @@ import "./App.css";
 function Home() {
   return (
     <>
-      <header className="flex justify-end items-center px-6 py-4 lg:py-2">
+      <header className="flex justify-end items-center px-6 py-4 lg:py-0">
         <AuthButtons />
       </header>
-      <span className="flex justify-center p-4 sm:p-8 mt-8 lg:mt-4 relative z-0">
+      <span className="flex justify-center p-4 sm:p-8 mt-8 lg:p-0 lg:mt-0 lg:mb-0 relative z-0">
         <Searchbar />
       </span>
       <Background />
@@ -30,7 +30,7 @@ function Home() {
 function App() {
   return (
     <div className="bg-black min-h-screen flex flex-col">
-      <div className="absolute top-4 left-1/2 transform -translate-x-1/2 z-[100] text-center sm:left-4 sm:transform-none md:left-4 md:transform-none lg:left-4 xl:left-4 sm:text-left md:text-left pointer-events-auto mb-72 lg:mb-6">
+      <div className="absolute top-4 left-1/2 transform -translate-x-1/2 z-[100] text-center sm:left-4 sm:transform-none md:left-4 md:transform-none lg:left-4 xl:left-4 sm:text-left md:text-left pointer-events-auto mb-72 lg:mb-0">
         <Link
           to="/"
           className="text-xl font-bold md:text-4xl hover:opacity-80 transition-opacity cursor-pointer inline-block relative z-[110] mb-10 lg:mb-0"
